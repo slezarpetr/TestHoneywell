7cda3478c902e6c17653b30d396b3fe14198e5a5
sleza <sleza@SLEZARJR-NTB>
2022-08-15 13:24:09 +0200

    Add project files.

---
+++ TestHoneywell/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestHoneywell;
+
+Tasks.TwoThrees();
+
+
+var inputNumbers1 = new List<int>(new[] { 5, 3, 3, 1, 0, 0, -2, -2, -5 });
+var inputNumbers2 = new List<int>(new[] { 1, 2, 3, 3, 3, 4, 4, 10, 13, 15, 15, 17 });
+var inputNumbers3 = new List<int>(new[] { 1, 2, 2 , 2, 3, 3, 3, 4, 4, 10, 13, 13, 15, 15, 17,18 });
+
+Console.WriteLine(Tasks.CountDuplicates(inputNumbers1));
+Console.WriteLine(Tasks.CountDuplicates(inputNumbers2));
+Console.WriteLine(Tasks.CountDuplicates(inputNumbers3));
+
---
+++ TestHoneywell/Tasks.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHoneywell
+{
+    public static class Tasks
+    {
+
+        public static int CountDuplicates(IList<int> numbers)
+        {
+            var originalLength = numbers.Count;
+            var asSet = new HashSet<int>(numbers);
+
+            return originalLength - asSet.Count;
+        }
+
+        public static void TwoThrees()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                string currentOutput;
+                if (i % 2 == 0)
+                {
+                    currentOutput = "two";
+
+                    if (i % 3 == 0)
+                    {
+                        currentOutput += "three";
+                    }
+                }
+                else if (i % 3 == 0)
+                {
+                    currentOutput = "three";
+                }
+                else
+                {
+                    currentOutput = i.ToString();
+                }
+
+                Console.WriteLine(currentOutput);
+            }
+        }
+    }
+}
